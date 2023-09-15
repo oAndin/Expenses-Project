2230b30b6decb7b48e6b8b53c3c9dc90733577a1
oAndin <00andherson@gmail.com>
2023-09-14 23:48:10 -0300

    Update Components

---
+++ src/Components/Card/Index.jsx
@@ -0,0 +1,12 @@
+import './Style.css';
+
+const Card = (props) => {
+    const classes = 'card ' + props.className;
+    return (
+        <>
+            <div className={classes}>{props.children}</div>
+        </>
+    );
+};
+
+export default Card;
--- src/Components/ExpenseItem/Index.jsx
+++ src/Components/ExpenseItem/Index.jsx
@@ -1,17 +1,17 @@
 import ExpenseDate from '../ExpenseDate/Index';
 import './ExpenseItem.css';
 import '../ExpensesList/ExpensesList.css'
-
+import Card from  '../Card/Index'
 const ExpenseItem = (props) => {
 
     return (
-            <div className="expense-item">
+            <Card className="expense-item">
             <ExpenseDate date={props.date} />
                 <div className="expense-item__description">
                     <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-            </div>
+            </Card>
     );
 };
 
